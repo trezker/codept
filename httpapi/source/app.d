65f4f6b295ecc76181e8a137f71ea8a914fceeb9
Anders Andersson <trezker@gmail.com>
2020-08-30 07:01:02 +0200

    Set up vibe

--- httpapi/source/app.d
+++ httpapi/source/app.d
@@ -1,6 +1,22 @@
-import std.stdio;
+import vibe.vibe;
+
+void index(HTTPServerRequest req, HTTPServerResponse res)
+{
+	Json json = Json.emptyObject;
+	json["success"] = true;
+	res.writeBody(serializeToJsonString(json), 200);
+}
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	auto router = new URLRouter;
+	router.get("/", &index);
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 8090;
+
+	listenHTTP(settings, router);
+
+	disableDefaultSignalHandlers();
+	runApplication();
 }
