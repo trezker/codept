d4d127dc3c572edae506447762d58f1626f089d8
Anders Andersson <trezker@gmail.com>
2020-08-30 12:23:01 +0200

    Reorg

---
+++ api/source/app.d
@@ -0,0 +1,22 @@
+import vibe.vibe;
+
+void index(HTTPServerRequest req, HTTPServerResponse res)
+{
+	Json json = Json.emptyObject;
+	json["success"] = true;
+	res.writeBody(serializeToJsonString(json), 200);
+}
+
+void main()
+{
+	auto router = new URLRouter;
+	router.get("/", &index);
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+
+	listenHTTP(settings, router);
+
+	disableDefaultSignalHandlers();
+	runApplication();
+}
