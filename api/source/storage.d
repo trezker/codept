771fb3daa6732dd15f762384a4c5900b7eef0985
Anders Andersson <trezker@gmail.com>
2020-09-10 19:05:33 +0200

    All the tests

--- api/source/storage.d
+++ api/source/storage.d
@@ -87,6 +87,10 @@ public:
 		try {
 			Test1();
 			storage.Reset();
+			Test2();
+			storage.Reset();
+			Test3();
+			storage.Reset();
 		}
 		catch(Exception e) {
 			writeln(e);
@@ -98,32 +102,33 @@ public:
 	void Test1() {
 		assert(0 == storage.LoadBacklog().length);
 	}
+
+	void Test2() {
+		Story story;
+		storage.SaveStory(story);
+		assert(1 == storage.LoadBacklog().length);
+	}
+
+	void Test3() {
+		Story story;
+		story.id = 0;
+		storage.SaveStory(story);
+		Story story2;
+		story2.id = 0;
+		storage.SaveStory(story2);
+		assert(1 == storage.LoadBacklog()[0].id);
+		assert(2 == storage.LoadBacklog()[1].id);
+
+		Story storyUpdate;
+		storyUpdate.id = 1;
+		storyUpdate.title = "Updated";
+		storage.SaveStory(storyUpdate);
+		assert(2 == storage.LoadBacklog().length);
+		assert("Updated" == storage.LoadBacklog()[0].title);
+	}
 };
 
-/*
 unittest {
-	Storage storage = new Storage;
-	Story story;
-	storage.SaveStory(story);
-	assert(1 == storage.LoadBacklog().length);
+	StorageTest s = new StorageTest;
+	s.Run();
 }
-
-unittest {
-	Storage storage = new Storage;
-	Story story;
-	story.id = 0;
-	storage.SaveStory(story);
-	Story story2;
-	story2.id = 0;
-	storage.SaveStory(story2);
-	assert(1 == storage.LoadBacklog()[0].id);
-	assert(2 == storage.LoadBacklog()[1].id);
-
-	Story storyUpdate;
-	storyUpdate.id = 1;
-	storyUpdate.title = "Updated";
-	storage.SaveStory(storyUpdate);
-	assert(2 == storage.LoadBacklog().length);
-	assert("Updated" == storage.LoadBacklog()[0].title);
-}
-*/
